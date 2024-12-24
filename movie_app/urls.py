e888d2a2094f910d6d78474d2e5b0526d7353dfc
iskenderovnaat <asema.tynymbekova@icloud.com>
2024-12-24 20:38:30 +0600

    added dz6

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -3,7 +3,7 @@ from movie_app import views
 
 urlpatterns = [
     path('movie_class/', views.MovieListAPIView.as_view(), name='movie_list'),
-    path('movie_class/<int:id>/', views.MovieDetailAPIView.as_view(), name='movie_detail'),
+    path('movie_class/<int:id>/', views.MovieDetaiglAPIView.as_view(), name='movie_detail'),
     path('director_class/', views.DirectorListAPIView.as_view(), name='director_list'),
     path('director_class/<int:id>/', views.DirectorDetailAPIView.as_view(), name='director_detail'),
     path('review_class/', views.ReviewListAPIView.as_view(), name='review_list'),
